bbbb143c51cf869af18a91f69269d10780930c4f
acf-patrick <miharisoap@gmail.com>
2026-01-25 12:38:47 +0300

    add raisin example

---
+++ examples/raisin/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"slices"
+
+	"github.com/bleak-and-bare/machine_learning/classification"
+	"github.com/bleak-and-bare/machine_learning/internal/dataset"
+	"github.com/bleak-and-bare/machine_learning/internal/iterable/accumulator"
+	"github.com/bleak-and-bare/machine_learning/internal/maths/regularization"
+	"github.com/bleak-and-bare/machine_learning/internal/maths/utils"
+	"github.com/bleak-and-bare/machine_learning/internal/misc"
+	"github.com/bleak-and-bare/machine_learning/internal/selector"
+	"github.com/bleak-and-bare/machine_learning/processing"
+	"github.com/bleak-and-bare/machine_learning/regression/linear"
+)
+
+func main() {
+	ds := dataset.NewDataSet[float32](7)
+	if err := ds.LoadCsv("./examples/dataset/Raisin_Dataset.csv", ','); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load dataset : %v", err)
+		return
+	}
+
+	ds.MapColumn("Class", func(dc dataset.DataCell) dataset.DataCell {
+		col, _ := dc.(*dataset.StrDataCell)
+		if col.Value == "Kecimen" {
+			return &dataset.RealDataCell[float32]{Value: 1.0}
+		}
+
+		return &dataset.RealDataCell[float32]{Value: 0.0}
+	})
+	ds.Shuffle()
+
+	// chunk, _ := ds.Extract(0.0, 0.125)
+	// ds = *chunk
+
+	train, _ := ds.Extract(0.0, 0.75)
+	test, _ := ds.Extract(0.75, 1.0)
+
+	scaler := processing.StandardScaler[float32]{}
+	cols := test.GetColumnNames()
+	for _, col := range cols {
+		if col != "Class" {
+			scaler.FitTransformDataSet(train, col)
+			scaler.TransformDataSet(test, col)
+		}
+	}
+
+	train.Head(5)
+
+	m := linear.NewLogisticReg[float32]()
+	m.Penalty = regularization.Ridge
+
+	gs := selector.NewGridSearch(map[string][]float64{
+		"lambda": utils.Logspace(-4, -2, 5),
+	}, linear.LogRegFactory(m, func(m map[string]float64) regularization.ElasticnetParams {
+		return regularization.ElasticnetParams{
+			Lambda: m["lambda"],
+		}
+	}))
+
+	if err := gs.Fit(train); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to search hyper params : %v", err)
+		return
+	}
+
+	best_params := gs.BestParams()
+	m.SetHyperParams(0.0, best_params["lambda"])
+
+	if err := m.Fit(train); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to train model : %v", err)
+		return
+	}
+
+	t := test.CollectTargets()
+	r := m.PredictOn(test)
+
+	var gp misc.GridPrinter
+	gp.Columns("Min", "Max", "Mean")
+	gp.NewRow()
+	gp.Columns(
+		fmt.Sprintf("%.3f", slices.Min(r)),
+		fmt.Sprintf("%.3f", slices.Max(r)),
+		fmt.Sprintf("%.3f", accumulator.Mean(slices.Values(r))),
+	)
+	gp.Print(true)
+
+	trg := make([]int, len(t))
+	pred := make([]int, len(r))
+
+	for i := range r {
+		trg[i] = int(t[i])
+		pred[i] = int(math.Round(float64(r[i])))
+	}
+
+	classification.ComputeReport(trg, pred)
+}
